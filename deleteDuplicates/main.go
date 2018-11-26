c3a3ae1272d15353277bf216ce7228b263f43c1d
simple.xull <simple.xull@gmail.com>
2018-11-26 22:08:59 +0800

    leetcode

---
+++ deleteDuplicates/main.go
@@ -0,0 +1,21 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+ type ListNode struct {
+ 	Val int
+ 	Next *ListNode
+ }
+
+func deleteDuplicates(head *ListNode) *ListNode {
+
+
+	return head
+}
+
