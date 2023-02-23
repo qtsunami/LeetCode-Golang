d8636d8898d0c47083a1124cf2435e5e6aa46081
qtsunami <xuyanlu0424@163.com>
2023-02-23 19:02:57 +0800

    add-binary

---
+++ 67.add-binary/main.go
@@ -0,0 +1,20 @@
+package main
+
+// https://leetcode.cn/problems/add-binary/
+// 67. 二进制求和
+// 给你两个二进制字符串 a 和 b ，以二进制字符串的形式返回它们的和。
+// 示例 1： 输入:a = "11", b = "1"  输出："100"
+// 示例 2：  输入：a = "1010", b = "1011"  输出："10101"
+// 提示：
+// 1 <= a.length, b.length <= 104
+// a 和 b 仅由字符 '0' 或 '1' 组成
+// 字符串如果不是 "0" ，就不含前导零
+
+func main() {
+
+}
+
+func addBinary(a string, b string) string {
+
+	return ""
+}
